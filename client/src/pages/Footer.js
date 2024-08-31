372d663d3e9acc50d4e1e0617675bda127a1836b
Abhishek <abhishek9872615721@gmail.com>
2024-08-31 17:04:26 +0530

    Abhishek

---
+++ client/src/pages/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div class="footer">
+      <p>&copy; Copyright 2022 - <a href="https://linkedin.com." target="blank">Daily Work Hub </a> <span>&nbsp; &nbsp;&nbsp; </span>
+        <a
+          href="https://github.com"><i
+            class="fa-brands fa-github"></i></a><span>&nbsp;&nbsp; &nbsp; </span>
+        <a
+          href="https://linkedin.com"><i
+            class="fa-brands fa-linkedin"></i></a><span>&nbsp;&nbsp; &nbsp; </span>
+        <a
+          href="#"><i class="fa-brands fa-instagram"></i></a></p>
+    </div>
+  )
+}
+
+export default Footer
